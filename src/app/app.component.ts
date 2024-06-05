b8b5eb3d3c11a25f69082a0ddcc3ea4cbcea6375
Alina7121 <113956800+Alina7121@users.noreply.github.com>
2024-06-05 13:48:44 -0400

    added meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'zhara';
+export class AppComponent implements OnInit {
+
+  constructor(private meta: Meta) {}
+
+  ngOnInit(): void {
+    this.meta.updateTag({ name: 'description', content: 'Welcome to Zhara Music Festival, a vibrant celebration of culture and music.' });
+    this.meta.updateTag({ name: 'keywords', content: 'music, festival, culture, celebration, Zhara' });
+  }
 }

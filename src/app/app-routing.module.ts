ac29354789a9d91827c43fd002694ff82b27b1d4
Alina7121 <113956800+Alina7121@users.noreply.github.com>
2024-06-09 10:41:29 -0400

    changed volinteer page text

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TicketsComponent } from './tickets/tickets.component';
 import { AboutComponent } from './about/about.component';
-import { GalleryComponent } from './gallery/gallery.component';
 import { FaqComponent } from './faq/faq.component';  // Corrected here
 import { ScheduleComponent } from './schedule/schedule.component';
 import { GetInvolvedComponent } from './get-involved/get-involved.component';
@@ -13,8 +12,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'tickets', component: TicketsComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'gallery', component: GalleryComponent },
-  { path: 'faq', component: FaqComponent },  // Corrected here
+    { path: 'faq', component: FaqComponent },  // Corrected here
   { path: 'schedule', component: ScheduleComponent },
   { path: 'get-involved', component: GetInvolvedComponent },
   { path: 'contact-us', component: ContactUsComponent },
